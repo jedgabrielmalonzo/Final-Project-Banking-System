17397ef0230fbc6f683011252c1b080cb75b3056
jedgabrielmalonzo <qjgmalonzo@tip.edu.ph>
2024-10-14 02:44:14 +0800

    Add files via upload

---
+++ Account_Statement.java
@@ -0,0 +1,43 @@
+package Account_Statement;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Account_Statement {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Account_Statement window = new Account_Statement();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Account_Statement() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 876, 540);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
